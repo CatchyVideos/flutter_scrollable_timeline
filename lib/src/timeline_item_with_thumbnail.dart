a85ff374dd12aea24bdf14759a86c53509c3d876
JulesFleuren <95241984+JulesFleuren@users.noreply.github.com>
2024-05-08 12:41:13 +0200

    added implementation for timeline with thumbnails

---
+++ lib/src/timeline_item_with_thumbnail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'timeline_item_data.dart';
+
+class TimelineItemWithThumbnail extends StatelessWidget {
+  final TimelineItemData curItem;
+  // potentially independent bg color for each item
+  final Color backgroundColor;
+  final double rulerOutsidePadding;
+  final double rulerSize;
+  final double rulerInsidePadding;
+  final ThumbnailProvider thumbnailProvider;
+
+  TimelineItemWithThumbnail(this.curItem, this.backgroundColor,
+      this.rulerOutsidePadding, this.rulerSize, this.rulerInsidePadding,
+      this.thumbnailProvider, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 1,
+            vertical: rulerOutsidePadding,
+          ),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          // outside of the range of the timeline, curItem.t will be -1 and an empty container should be returned
+          child: (curItem.t >= 0) ? thumbnailProvider.thumbnail(curItem.tSecs!) : new Container()
+    ));
+  }
+}
+
+abstract class ThumbnailProvider {
+  Image thumbnail(int seconds);
+}
