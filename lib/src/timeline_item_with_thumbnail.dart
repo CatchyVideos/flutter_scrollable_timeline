d280bb2599dd04c2d7bce19f3f802758fbfb7084
JulesFleuren <95241984+JulesFleuren@users.noreply.github.com>
2024-05-09 00:04:53 +0200

    refactor seconds -> index

--- lib/src/timeline_item_with_thumbnail.dart
+++ lib/src/timeline_item_with_thumbnail.dart
@@ -41,5 +41,5 @@ class TimelineItemWithThumbnail extends StatelessWidget {
 }
 
 abstract class ThumbnailProvider {
-  ImageProvider thumbnail(int seconds);
+  ImageProvider thumbnail(int index);
 }
